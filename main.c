11f4166f217d842440bf484c5f559139c75e76c4
andreeani <andreeani@yahoo.com>
2017-03-31 07:15:25 +0000

    Added main.c added function hello1()

--- main.c
+++ main.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void hello1(void)
-{
-	printf(stdout, "This literal is displayed on standard output\n");
+void hello1(void){
+printf(stdout, "This literal is displayed on standard output\n");
 }
 
-int main(void)
+int main (void)
 {
-	hello1();
-
-	return EXIT_SUCCESS;
+hello1();
+return EXIT_SUCCESS;
 }
+
